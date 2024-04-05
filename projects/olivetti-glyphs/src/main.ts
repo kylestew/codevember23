06e1554a22860b1992158db6804dfc15d1dc53ed
Kyle Stewart <kylestew@gmail.com>
2024-04-05 12:15:14 +0100

    Iterating, but not really

--- projects/olivetti-glyphs/src/main.ts
+++ projects/olivetti-glyphs/src/main.ts
@@ -1,12 +1,11 @@
 import { parse as parseFont } from 'opentype.js'
 import { parse as parseXML, Type } from '@thi.ng/sax'
-import { pathFromSvg, asPolygon, vertices, circle, Circle, asSvg, svgDoc } from '@thi.ng/geom'
-import { comp, mapcat, transduce, map, flatten, filter, push, range } from '@thi.ng/transducers'
+import { Circle, circle, asPolygon, pathFromSvg, vertices, svgDoc, asSvg, intersects } from '@thi.ng/geom'
+import { comp, mapcat, transduce, map, flatten, filter, push, range, pluck } from '@thi.ng/transducers'
 import { pickRandom } from '@thi.ng/random'
 import { Vec, dist } from '@thi.ng/vectors'
 
 import fontFileURL from './assets/FiraSansOT-Medium.otf?url'
-
 async function loadFont() {
     const response = await fetch(fontFileURL)
     const buffer = await response.arrayBuffer()
@@ -15,11 +14,11 @@ async function loadFont() {
 }
 const font = await loadFont()
 
-const circleRad = 4
+type GlyphPlacement = { letter: string; pos: Vec; fontSize: number; circles: Circle[] }
 
-// one letter to circles
-function circlesFromLetter(letter: string, x: number, y: number, fontSize: number): Circle[] {
-    return transduce(
+function createGlyphPlacement(letter: string, x: number, y: number, fontSize: number): GlyphPlacement {
+    const circleRad = 5.0
+    const circles = transduce(
         comp(
             // opentype to SVG string
             mapcat((letter) => font.getPath(letter, x, y, fontSize).toSVG()),
@@ -29,7 +28,7 @@ function circlesFromLetter(letter: string, x: number, y: number, fontSize: numbe
             filter((ev) => ev.type === Type.ELEM_END && ev.tag === 'path'),
             // convert path strings to geom.Path objects
             mapcat((ev) => pathFromSvg(ev.attribs!.d)),
-            // convert paths to vertices
+            // convert paths to polygons to vertices
             mapcat((path) => vertices(asPolygon(path), { dist: circleRad * 2 })),
             // convert vertices to circles
             map((p) => circle(p, circleRad))
@@ -37,42 +36,16 @@ function circlesFromLetter(letter: string, x: number, y: number, fontSize: numbe
         push<Circle>(),
         [letter]
     )
-}
-
-class CirclePacker {
-    width: number
-    height: number
-
-    // TODO: speed up using a data structure like a quadtree
-    // Try and use a non-circle packing technique, GEOM has some intersection functions
-
-    private items: Circle[] = []
-
-    constructor(width: number, height: number) {
-        this.width = width
-        this.height = height
-    }
-
-    private canAddCircle(circle: Circle): boolean {
-        return this.items.every((item) => dist(circle.pos, item.pos) > circle.r + item.r)
-    }
-
-    canAddCircles(circles: Circle[]): boolean {
-        for (const c of circles) {
-            if (!this.canAddCircle(c)) {
-                return false
-            }
-        }
-        return true
-    }
-
-    add(circles: Circle[]): void {
-        this.items.push(...circles)
+    return {
+        letter,
+        pos: [x, y],
+        fontSize,
+        circles,
     }
 }
 
-const ATTEMPTS = 8000
-const MIN_FONT_SIZE = 24
+const ATTEMPTS = 2000
+const MIN_FONT_SIZE = 12
 const MAX_FONT_SIZE = 256
 const FONT_SIZE_STEP = 4
 const CANVAS_WIDTH = 1200
@@ -84,52 +57,66 @@ const characterSet = transduce(
         mapcat((char) => String.fromCharCode(char)) //
     ),
     push<string>(),
+    // numbers, capital letters, lowercase letters
     [range(48, 57 + 1), range(65, 90 + 1), range(97, 122 + 1)]
 )
 
-const circlePacker = new CirclePacker(CANVAS_WIDTH, CANVAS_HEIGHT)
-type PlacedLetter = { letter: string; pos: Vec; fontSize: number }
-const placedLetters: PlacedLetter[] = []
+const circleField: Circle[] = []
+const placedGlyphs: GlyphPlacement[] = []
+function canAddGlyph(glyph: GlyphPlacement): boolean {
+    return glyph.circles.every((circle) => {
+        return circleField.every((placed) => dist(circle.pos, placed.pos) > circle.r + placed.r)
+    })
+}
 
 // pack them!
 for (let i = 0; i < ATTEMPTS; i++) {
-    // try to place one shape
-    var fontSize = MIN_FONT_SIZE
+    // try to place one glyph
+    const letter = pickRandom(characterSet)
     let x = Math.random() * CANVAS_WIDTH
     let y = Math.random() * CANVAS_HEIGHT
+    let fontSize = MIN_FONT_SIZE
 
-    // select random character from characterSet
-    const letter = pickRandom(characterSet)
-    let circles = circlesFromLetter(letter, x, y, fontSize)
-    let placement: PlacedLetter | undefined
+    let glyphPlacement: GlyphPlacement | undefined
 
-    while (circlePacker.canAddCircles(circles) && fontSize <= MAX_FONT_SIZE) {
-        placement = { letter, pos: [x, y], fontSize }
+    let glyphAttempt = createGlyphPlacement(letter, x, y, fontSize)
+    while (canAddGlyph(glyphAttempt) && fontSize <= MAX_FONT_SIZE) {
+        glyphPlacement = glyphAttempt
 
         // attempt to step it up
         fontSize += FONT_SIZE_STEP
-        circles = circlesFromLetter(letter, x, y, fontSize)
+        glyphAttempt = createGlyphPlacement(letter, x, y, fontSize)
     }
 
-    // we're we able to place the letter?
-    if (placement) {
-        circlePacker.add(circles)
-        placedLetters.push(placement)
+    // we're we able to place the glyph?
+    if (glyphPlacement) {
+        placedGlyphs.push(glyphPlacement)
+        circleField.push(...glyphPlacement.circles)
     }
 }
 
+console.log('placed glyphs:', placedGlyphs.length)
+
 // display them!
-const svgPathElements = transduce(
+let svgContent = `<svg width="${CANVAS_WIDTH}" height="${CANVAS_HEIGHT}" xmlns="http://www.w3.org/2000/svg">`
+svgContent += `<g fill="black">`
+svgContent += transduce(
     map(({ letter, pos, fontSize }) => font.getPath(letter, pos[0], pos[1], fontSize).toSVG()),
     push(),
-    placedLetters
-)
-console.log('placed', svgPathElements.length, 'letters')
-
-// wrap path elements in SVG doc
-let svgStart = `<svg width="${CANVAS_WIDTH}" height="${CANVAS_HEIGHT}" xmlns="http://www.w3.org/2000/svg">`
-let svgEnd = `</svg>`
-let svgPaths = svgPathElements.join('\n')
-let svgContent = `${svgStart}\n${svgPaths}\n${svgEnd}`
+    placedGlyphs
+).join('\n')
+svgContent += `</g>`
+// svgContent += `<g fill="none" stroke="red" stroke-width="1">`
+// svgContent += transduce(
+//     comp(
+//         pluck('circles'),
+//         flatten(),
+//         map((circle) => asSvg(circle))
+//     ),
+//     push(),
+//     placedGlyphs
+// ).join('\n')
+// svgContent += `</g>`
+svgContent += `</svg>`
 
 document.body.innerHTML = svgContent
