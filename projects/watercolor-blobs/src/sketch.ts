335632c9519aafbdb715fedb84a393f56bb32bb8
Kyle Stewart <kylestew@gmail.com>
2024-04-14 14:22:52 +0100

    ...

--- projects/watercolor-blobs/src/sketch.ts
+++ projects/watercolor-blobs/src/sketch.ts
@@ -4,8 +4,9 @@ import tinycolor from 'tinycolor2'
 import { draw } from '@thi.ng/hiccup-canvas'
 import { circle, Polygon, asPolygon, points, edges, line, arcLength, Line, pointAt, polygon } from '@thi.ng/geom'
 import { transduce, comp, push, trace, map, mapcat, flatten1 } from '@thi.ng/transducers'
-import { Vec, vec2, Vec2, VecPair, perpendicularCW, mag, normalize, add, mulN } from '@thi.ng/vectors'
-import { SYSTEM } from '@thi.ng/random'
+import { Vec, vec2, Vec2, VecPair, perpendicularCW, mag, normalize, add, mulN, rotate } from '@thi.ng/vectors'
+import { SYSTEM, gaussian } from '@thi.ng/random'
+import { clamp01 } from '@thi.ng/math'
 
 function asVec2(p: VecPair): Vec2 {
     return vec2(p[1][0] - p[0][0], p[1][1] - p[0][1])
@@ -22,19 +23,19 @@ export class MySketch extends Sketch {
     constructor(params: SketchParams, appElm: HTMLElement) {
         super(params, 100)
 
-        // this.dpr = window.devicePixelRatio || 1
+        const dpr = window.devicePixelRatio || 1
         const { ctx } = adaptiveCanvas2d(this.width, this.height, appElm)
         this.ctx = ctx
 
         // scale canvas so shortest side is in range [-1, 1]
         if (this.width > this.height) {
-            const scale = this.height / 2.0
+            const scale = (this.height / 2.0) * dpr
             this.ctx.scale(scale, scale)
-            this.ctx.translate((this.width * 0.5) / scale, 1.0)
+            this.ctx.translate((this.width * 0.5 * dpr) / scale, 1.0)
         } else {
-            const scale = this.width / 2.0
+            const scale = (this.width / 2.0) * dpr
             this.ctx.scale(scale, scale)
-            this.ctx.translate(1.0, (this.height * 0.5) / scale)
+            this.ctx.translate(1.0, (this.height * 0.5 * dpr) / scale)
         }
     }
 
@@ -51,23 +52,32 @@ export class MySketch extends Sketch {
         )
     }
 
-    update(iteration: number): number {
-        // TODO: create the points yourself
-        const shape = this.shapeGen.next().value
+    randomEdgeBreak = () => {
+        // gaussian ditro centered around 0.5 with a scale of 2
+        const gauss = gaussian(SYSTEM, 24, 0.5, 2.0)
+        return clamp01(gauss())
+    }
+    randomAngle = () => {
+        return gaussian(SYSTEM, 24, 0.0, Math.PI)()
+    }
+    randomLength = (edgeLength: number) => {
+        return Math.abs(gaussian(SYSTEM, 24, 0.0, 2.0)()) * edgeLength
+    }
 
+    iterateCoastline(poly: Polygon): Polygon {
         const points = transduce(
             comp(
                 map((edge) => {
                     // cut the edge at some point near the middle
-                    const breakPt = pointAt(line(edge), 0.5)!
+                    const breakPt = pointAt(line(edge), randomEdgeBreak())!
                     // determine vector perpendicular to edge
                     const vecAB = asVec2(edge)
-                    const perp = perpendicularCW([], vecAB)
+                    let perp = perpendicularCW([], vecAB)
                     const length = mag(vecAB)
-                    // distrub the angle
-                    // TODO:
+                    // adjust the angle a random amount
+                    rotate(perp, perp, randomAngle())
                     // project the break point outwards
-                    const proj = mulN([], normalize(null, perp), length * 0.5)
+                    const proj = mulN([], normalize(null, perp), randomLength(length))
                     const pt = add([], breakPt, proj)
 
                     // return new edge (2nd edge is defined by end of this edge and start of next)
@@ -77,21 +87,46 @@ export class MySketch extends Sketch {
                 // trace()
             ),
             push<Vec>(),
-            edges(shape)
+            edges(poly)
         )
-        const poly = polygon(points, shape.attribs)
+        return polygon(points, poly.attribs)
+    }
 
-        // (1) break the edge at some random point in the middle of each edge
-        // (2) angle of break
-        // (3) magnitude edge extends outwards
-        // NOTE: seems like the angle of break can define the magnitude (3)
+    update(iteration: number): number {
+        // TODO: create the points yourself
+        const shape = this.shapeGen.next().value
 
-        // const circ = circle([this.width / 2, this.height / 2], 100, { fill: tinycolor(this.params.tint).toRgbString() })
-        // const poly = asPolygon(rotate(circ, 0.1))
-        // let poly = asPolygon(circ, 12)
-        // poly = rotate(poly, Math.PI / 8.0)
+        // assign base probabilities
+        const edgeware = [
+            ...map((edge: VecPair) => {
+                edge.attr = { abc: 123 }
+                return edge
+            }, edges(shape)),
+        ]
+        const newShape = polygon(edgeware)
+        console.log(newShape)
 
-        this.shapes.push(poly)
+        // const edge0 = [...edges(shape)][0]
+        // console.log(edge0)
+        // console.log(...edges(shape))
+        //...
+
+        // create a base subdivided poly
+        // let poly = this.oneRound(shape)
+        // poly = this.oneRound(poly)
+        // poly = this.oneRound(poly)
+        // const baseShape = this.oneRound(poly)
+
+        // Create 50 version of this subdivided 3 more times
+        // for (let i = 0; i < 100; i++) {
+        //     let newPoly = this.oneRound(baseShape)
+        //     newPoly = this.oneRound(baseShape)
+        //     newPoly = this.oneRound(baseShape)
+        //     this.shapes.push(newPoly)
+        // TODO: push the color with a opacity scaled to # of iterations
+        // }
+
+        // this.shapes.push(poly)
 
         this.stop()
         return iteration
@@ -100,5 +135,6 @@ export class MySketch extends Sketch {
     render() {
         const bgColor = tinycolor(this.params.background)
         draw(this.ctx, ['g', { __background: bgColor.toRgbString() }, ...this.shapes])
+        // TODO: draw with masking for texture
     }
 }
