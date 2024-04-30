281126b719f3fbe523ac435680da97d7f638ac87
Kyle Stewart <kylestew@gmail.com>
2024-04-30 22:42:05 +0100

    ...

---
+++ libs/geometry/blob.ts
@@ -0,0 +1,44 @@
+import { bounds, edges, Circle, circle, line, scatter, pointAt, group } from '@thi.ng/geom'
+import { iterator, comp, flatten1, map, trace, partition } from '@thi.ng/transducers'
+import { rotate } from '@thi.ng/arrays'
+
+export function blobFromCircle(circ: Circle, randRadius: number): Path2D {
+    // circle -> bounding rect -> edges -> control points
+    let pts = [
+        ...iterator(
+            comp(
+                // disturb edge points a bit
+                map((pts) => [scatter(circle(pts[0], randRadius), 1)![0], scatter(circle(pts[1], randRadius), 1)![0]]),
+                // map((pts) => pts),
+                // turn into lines
+                map((pts) => line(pts[0], pts[1])),
+                // find modpoint of line (becomes new control point)
+                map((line) => [pointAt(line, 0.22), pointAt(line, 0.5), pointAt(line, 0.77)]),
+                // just the points
+                flatten1()
+                // trace() //
+            ),
+            // edges of bounding rect of circle
+            edges(bounds(circ))
+        ),
+    ]
+
+    // first point is last control point, rotate left
+    pts = rotate(pts, -1)
+    // move to that control point
+    const firstPt = pts[0]!
+    // now rotate to be last point in array
+    pts = rotate(pts, -1)
+
+    // convert points to Path2D
+    let path = new Path2D()
+    path.moveTo(firstPt[0], firstPt[1])
+
+    const groupedPoints = [...iterator(partition(3), pts)]
+    groupedPoints.forEach((pts) => {
+        const [p1, p2, p3] = pts
+        path.bezierCurveTo(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1])
+    })
+    path.closePath()
+    return path
+}
--- sketches/_template/sketch.js
+++ sketches/_template/sketch.js
@@ -1,26 +1,36 @@
 import { createCanvas, setCanvasRange } from 'canvas-utils'
-import { circle, polygon, vertices } from '@thi.ng/geom'
-import { iterator, comp, trace, mapIndexed, partition } from '@thi.ng/transducers'
+import { circle } from '@thi.ng/geom'
+import { blobFromCircle } from './libs/geometry/blob'
 import { draw } from '@thi.ng/hiccup-canvas'
+import { iterator, comp, trace, map, range2d } from '@thi.ng/transducers'
+import {} from '@thi.ng/arrays'
+import { add } from '@thi.ng/vectors'
 
-// define polygon wedges
-const WEDGE_COUNT = 24
-const coloredWedges = iterator(
-    comp(
-        // partition pairs of points
-        partition(2, 1, true),
-        // trace(),
-        // convert to polygons and color them
-        mapIndexed((idx, pt_pair) => {
-            const pt0 = pt_pair[0]
-            const pt1 = pt_pair[1] ?? [1, 0]
-            return polygon([pt0, pt1, [0, 0]], { fill: `hsl(${(idx / WEDGE_COUNT) * 360}, 100%, 50%)` })
-        })
-    ),
-    // vertices around a circle
-    vertices(circle([0, 0], 1), WEDGE_COUNT + 1)
-)
+// draw(ctx, ['g', { __background: '#333344' }, ...coloredWedges])
 
-const ctx = createCanvas(800, 600, 'sketchCanvas')
-setCanvasRange(ctx, -1.1, 1.1)
-draw(ctx, ['g', { __background: '#333344' }, ...coloredWedges])
+const ctx = createCanvas(1200, 1200, 'sketchCanvas')
+ctx.background('#333344')
+setCanvasRange(ctx, -1.2, 1.2)
+
+for (let i = 0; i < 12; i++) {
+    const blobs = iterator(
+        comp(
+            // create circle at grid point
+            map((pt) => circle(add([], pt, [0.25, 0.25]), 0.2)),
+            // convert to blob
+            map((circ) => blobFromCircle(circ, i / 30.0)),
+            trace() //
+        ),
+        // layout grid
+        range2d(-1.0, 1.0, -1.0, 1.0, 0.5, 0.5)
+    )
+
+    // draw blobs
+    ctx.fillStyle = '#ee669911'
+    ctx.strokeStyle = '#ffffff98'
+    for (const blob of blobs) {
+        ctx.fill(blob)
+        ctx.lineWidth = 0.006
+        ctx.stroke(blob)
+    }
+}
