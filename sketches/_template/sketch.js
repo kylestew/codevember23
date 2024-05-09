c0b8588a5357033608707f6ec9e7c97a4d5cd356
Kyle Stewart <kylestew@gmail.com>
2024-05-10 00:35:45 +0100

    Drawing command

--- sketches/_template/sketch.js
+++ sketches/_template/sketch.js
@@ -1,24 +1,20 @@
 import { createCanvas, setCanvasRange } from './tools/canvas-utils'
 import { Circle, Polygon, asPath, vertices } from './tools/geo'
 import { partition } from './tools/array'
+import { draw } from './tools/draw'
 
 // circle -> vertices -> partition into pairs -> map to polygons using center point
-const WEDGE_COUNT = 8
+const WEDGE_COUNT = 24
 const circ = new Circle([0, 0], 1)
 const verts = vertices(circ, WEDGE_COUNT)
 verts.push(verts[0]) // loop back to the start
-const polys = partition(verts, 2, 1, false).map((pair, idx) => {
-    const pt0 = pair[0]
-    const pt1 = pair[1] ?? [1, 0]
-    return new Polygon([pt0, pt1, [0, 0]], { fill: `hsl(${(idx / WEDGE_COUNT) * 360}, 100%, 50%)` })
-})
+const polys = partition(verts, 2, 1, false).map(
+    (pair, idx) => new Polygon([pair[0], pair[1], [0, 0]], { fill: `hsl(${(idx / WEDGE_COUNT) * 360}, 100%, 50%)` })
+)
 
 const ctx = createCanvas(800, 600)
 ctx.background('#333344')
 setCanvasRange(ctx, -1.1, 1.1)
 
-// TODO: drawing function that applies attribs
-for (const poly of polys) {
-    ctx.fillStyle = poly.attribs.fill
-    ctx.fill(asPath(poly))
-}
+draw(ctx, polys)
+draw(ctx, circ, { stroke: '#ffffff', weight: 0.05 })
---
+++ tools/draw/index.js
@@ -0,0 +1,48 @@
+import { asPath } from '../geo'
+
+export function draw(ctx, geo, attribs = {}) {
+    ctx.save()
+
+    // if an array, run draw on each element
+    if (Array.isArray(geo)) {
+        geo.forEach((g) => draw(ctx, g, attribs))
+        return
+    }
+
+    let hasStroke = false
+    let hasFill = false
+    if (attribs.fill && attribs.fill !== 'none') {
+        ctx.fillStyle = attribs.fill
+        hasFill = true
+    }
+    if (attribs.stroke) {
+        ctx.strokeStyle = attribs.stroke
+        hasStroke = true
+    }
+    if (attribs.weight) {
+        ctx.lineWidth = attribs.weight
+    }
+
+    // geo attribs override
+    if (geo.attribs.fill && geo.attribs.fill !== 'none') {
+        ctx.fillStyle = geo.attribs.fill
+        hasFill = true
+    }
+    if (geo.attribs.stroke) {
+        ctx.strokeStyle = geo.attribs.stroke
+        hasStroke = true
+    }
+    if (geo.attribs.weight) {
+        ctx.lineWidth = geo.attribs.weight
+    }
+
+    // perform drawing
+    if (hasFill) {
+        ctx.fill(asPath(geo))
+    }
+    if (hasStroke) {
+        ctx.stroke(asPath(geo))
+    }
+
+    ctx.restore()
+}
